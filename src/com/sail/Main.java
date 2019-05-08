260b9fe4f92613c6f8fba4f186de41bcf5816faf
saaaaaail <649014081@qq.com>
2019-05-08 21:35:02 +0800

    华为2019/5/8笔试题

--- src/com/sail/Main.java
+++ src/com/sail/Main.java
@@ -10,34 +10,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int count=1;
-        InputStream inputStream = System.in;
-        InputStreamReader reader = new InputStreamReader(inputStream);
-        BufferedReader br = new BufferedReader(reader);
-
-        boolean f = true;
-        while(f){
-            try {
-                String str = br.readLine();
-                System.out.println(str);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
         }
-
-        MyThread myThread = new MyThread(count);
-        Queue<Integer> q = new ArrayDeque();
-        q.add(1);
-        int ii = q.remove();
-        Queue<Integer> queue = new PriorityQueue<>();
-        Set set = new HashSet();
-        set.size();
-        set.contains(1);
-        set.add(1);
-        Map map = new HashMap();
-        map.size();
-        List list = new ArrayList(set);
     }
-
 }
